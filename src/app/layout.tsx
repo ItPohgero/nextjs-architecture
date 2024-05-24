525824868f07d6464b5220726302ac0716a3c53e
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-05-24 21:36:03 +0700

    shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import { ProviderReduxToolkit } from "@/configs/provider/Redux";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/style/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -17,7 +22,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+			>
 				<ProviderReduxToolkit>{children}</ProviderReduxToolkit>
 			</body>
 		</html>
